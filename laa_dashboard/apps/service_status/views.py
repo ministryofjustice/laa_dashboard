4efac9195a5a774e4252a0b3095be08b715a6a28
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-04-30 20:57:21 +0100

    Tidy view_status view

--- laa_dashboard/apps/service_status/views.py
+++ laa_dashboard/apps/service_status/views.py
@@ -3,12 +3,39 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from .models import Service
 import requests
 
-# Create your views here.
 
 ok_status_codes = [302, 200]
+
+
+def get_status_code(url, verify=False):
+    # try:
+    #     r = requests.get(url, verify=verify, timeout=5)
+    #     result = r.status_code
+    # except:
+    #     print('Error making request')
+    #     result = 0
+
+    r = requests.get(url, verify=verify, timeout=5)
+    result = r.status_code
+
+    return result
+
+
+def eval_code(status_code):
+
+    local_ok_status_codes = [302, 200]
+
+    if status_code in local_ok_status_codes:
+        result = True
+    else:
+        result = False
+
+    return result
+
 
 
 @login_required
@@ -17,31 +44,12 @@ def view_status(request):
     print('view_status')
     print(str(request))
 
-    services = Service.objects.order_by('name')
-
-    statuses = {}
-
-    for service in services:
-
-        # try:
-        #     r = requests.get(service.url, verify=False, timeout=5)
-        # except:
-        #     print(service.url)
-
-        if True:
-        #if r.status_code in ok_status_codes:
-            statuses[service.name] = 'Up'
-        else:
-            statuses[service.name] = 'Down'
+    services = Service.objects.order_by('name').values()
 
     template = loader.get_template('service_status/service_status.html')
 
-    #context = {'statuses': statuses, }
+    context = RequestContext(request, {'services': services})
 
-    context = RequestContext(request, {'statuses': statuses, })
-    #
-
-    #
     return HttpResponse(template.render(context))
 
 
@@ -70,3 +78,70 @@ def ajax(request):
     return JsonResponse(statuses)
 
 
+# def ajax(request):
+#
+#     print('ajax')
+#     print(str(request))
+#
+#     services = Service.objects.order_by('name')
+#
+#     response = {}
+#
+#     for service in services:
+#         print(service.url)
+#         # status_code = get_status_code(service.url)
+#         r = requests.get(url, verify=verify, timeout=5)
+#         status_code = r.status_code
+#         response[service.name] = eval_code(status_code)
+#
+#     return JsonResponse(response)
+
+
+def check_service(request):
+
+    print('check_service')
+    print(str(request))
+
+    get = request.POST.get
+
+    service_name = get('name')
+
+    service = None
+
+    try:
+        service = Service.objects.get(name=service_name)
+    except MultipleObjectsReturned:
+        print('Multiple objects with name!')
+        response = {'error': 'Multiple services with same name'}
+    except ObjectDoesNotExist:
+        print('Object not found')
+        response = {'error': 'No service with given name'}
+
+    if service:
+        status_code = get_status_code(service.url)
+        response = {service.name: status_code}
+
+    return JsonResponse(response)
+
+
+
+# @login_required
+# def view_status(request):
+#
+#     print('view_status')
+#     print(str(request))
+#
+#     services = Service.objects.order_by('name')
+#
+#     statuses = {}
+#
+#     for service in services:
+#
+#         statuses[service.name] = 'Up'
+#
+#
+#     template = loader.get_template('service_status/service_status.html')
+#
+#     context = RequestContext(request, {'statuses': statuses, })
+#
+#     return HttpResponse(template.render(context))
