bcf59709ebee727761e9c0e66b1e3e60d448bcdb
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-05-02 07:17:53 +0100

    Simple table view working

--- laa_dashboard/apps/service_status/urls.py
+++ laa_dashboard/apps/service_status/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^check_all_services/', views.check_all_services),
     url(r'^edit_status/', views.edit_status),
     url(r'^view_status/', views.view_status),
-    url(r'^update_status/', views.update_status),   
+    url(r'^update_status/', views.update_status),
     url(r'^simple_table/', views.simple_table),
     url(r'^', views.view_services),
 
