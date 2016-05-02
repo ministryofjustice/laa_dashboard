11dc0a8f8d5649477b7bb175fc59275691bbd87e
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-05-02 21:19:16 +0100

    Service periodic check working

--- laa_dashboard/apps/service_status/tasks.py
+++ laa_dashboard/apps/service_status/tasks.py
@@ -23,33 +23,33 @@ def set_auto_status(status_code):
     return auto_status
 
 
-# @shared_task
-# @app.task
-# def auto_check():
-#
-#     sys.stdout = open('std_log.txt', 'w')
-#     sys.sterr = open('std_err.txt', 'w')
-#
-#     print('***************auto_check**************')
-#     services = Service.objects.order_by('name')
-#
-#     for service in services:
-#
-#         try:
-#             print('Getting ' + service.url)
-#             r = requests.get(service.url, verify=False, timeout=5)
-#         except Exception as e:
-#             print('**********Requests Error *******************:  ', e)
-#
-#         if r.status_code:
-#             service.last_status_code = r.status_code
-#             service.last_check_time = datetime.now()
-#             service.auto_status = set_auto_status(r.status_code)
-#         else:
-#             service.last_status_code = 0
-#             service.last_check_time = timezone.now()
-#
-#         service.save()
+@app.task
+def auto_check():
+
+    sys.stdout = open('std_log.txt', 'w')
+    sys.sterr = open('std_err.txt', 'w')
+
+    print('***************auto_check**************')
+    services = Service.objects.order_by('name')
+
+    for service in services:
+
+        try:
+            print('Getting ' + service.url)
+            r = requests.get(service.url, verify=False, timeout=5)
+        except Exception as e:
+            print('**********Requests Error *******************:  ', e)
+
+        if r.status_code:
+            service.last_status_code = r.status_code
+            service.last_check_time = datetime.now()
+            service.auto_status = set_auto_status(r.status_code)
+        else:
+            service.last_status_code = 0
+            service.last_check_time = timezone.now()
+
+        service.save()
+
 
 @app.task
 def tta():
