10f480d9992bcdc292c99fd3290416fb8f4ed63e
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-05-05 01:45:32 +0100

    Javascript working

--- laa_dashboard/apps/service_status/static/service_status/main.js
+++ laa_dashboard/apps/service_status/static/service_status/main.js
@@ -78,71 +78,90 @@ function get_statuses() {
   });
 }
 
-function update_status_elements(response) {
+// function update_status_elements(response) {
+//
+//   console.log('***********update_status_elements called*******************')
+//   console.log(response);
+//
+//   $.each(response, function( index, value ) {
+//
+//     var row;
+//     var auto_cell;
+//     var manual_cell;
+//     var to_unset;
+//
+//     row = document.getElementById(value.name);
+//
+//     auto_cell = row.getElementsByClassName("auto_status_cell")[0];
+//     manual_cell = row.getElementsByClassName("manual_status_cell")[0];
+//
+//
+//     if (auto_cell != undefined) {
+//       if ($(auto_cell).hasClass(value.auto_status)) {
+//
+//       } else {
+//
+//         if (value.auto_status = true) {
+//           to_unset = false;
+//         } else {
+//           to_unset = true;
+//         }
+//
+//         auto_cell.classList.remove(to_unset.toString());
+//         auto_cell.classList.add(value.auto_status.toString());
+//
+//       }
+//     }
+//
+//     if (manual_cell != undefined) {
+//       if ($(manual_cell).hasClass(value.manual_status)) {
+//
+//       } else {
+//
+//         if (value.manual_status = true) {
+//           to_unset = false;
+//         } else {
+//           to_unset = true;
+//         }
+//
+//         manual_cell.classList.remove(to_unset.toString());
+//         manual_cell.classList.add(value.manual_status.toString());
+//
+//       }
+//     }
+//
+//
+//   });
+//
+// }
 
-  console.log('***********update_status_elements called*******************')
-  console.log(response);
+function update_status_elements(response, class_name) {
 
   $.each(response, function( index, value ) {
 
-    var row;
-    var auto_cell;
-    var manual_cell;
-    var to_unset;
+    var named_elements = document.getElementsByClassName(value.name);
+    var auto_status = value.auto_status;
+    var manual_status = value.manual_status;
 
-    row = document.getElementById(value.name);
+    $.each(named_elements, function( index, value ) {
 
-    auto_cell = row.getElementsByClassName("auto_status_cell")[0];
-    manual_cell = row.getElementsByClassName("manual_status_cell")[0];
-
-
-    if (auto_cell != undefined) {
-      if ($(auto_cell).hasClass(value.auto_status)) {
-
-      } else {
-
-        if (value.auto_status = true) {
-          to_unset = false;
-        } else {
-          to_unset = true;
-        }
-
-        auto_cell.classList.remove(to_unset.toString());
-        auto_cell.classList.add(value.auto_status.toString());
-
+      if ($(value).hasClass("auto_cell")) {
+        update_colour_class(value, response)
       }
-    }
 
-    if (manual_cell != undefined) {
-      if ($(manual_cell).hasClass(value.manual_status)) {
-
-      } else {
-
-        if (value.manual_status = true) {
-          to_unset = false;
-        } else {
-          to_unset = true;
-        }
-
-        manual_cell.classList.remove(to_unset.toString());
-        manual_cell.classList.add(value.manual_status.toString());
-
-      }
-    }
+    });
 
 
   });
 
 }
 
-
-
 function update_colour_class(target_element, colour) {
 
   var colours = ['red', 'amber', 'green']
 
   for (var i = 0; i < colours.length; i++) {
-    if ($(target_element).hasClass(colours[i])) {
+    if ($(target_element).hasClass(colours[i]) && colours[i] != colour) {
       target_element.classList.remove(colours[i]);
     }
   }
@@ -151,11 +170,19 @@ function update_colour_class(target_element, colour) {
 
 }
 
-function set_select_box_class() {
+function updateSelectedColours() {
   console.log("Change registered");
   var colour_class = event.target.value;
   update_colour_class(event.target, colour_class);
+  update_colour_class(event.target.parentElement, colour_class);
   set_cell_colours();
+}
+
+function initial_select_box_colour_class() {
+  var select_boxes = document.getElementsByTagName("select");
+  for (var i = 0; i < select_boxes.length; i++) {
+    update_colour_class(select_boxes[i], select_boxes[i].value)
+  }
 }
 
 function add_select_box_events() {
@@ -163,7 +190,7 @@ function add_select_box_events() {
   var select_boxes =  document.getElementsByTagName("select");
 
   for (var i = 0; i < select_boxes.length; i++) {
-    select_boxes[i].onchange = set_select_box_class;
+    select_boxes[i].onchange = updateSelectedColours;
   }
 
 }
